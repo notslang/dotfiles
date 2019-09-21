1786ff3e9ce7c52b9d095e05327ba84f06b558bc
Sean Lang <slang800@gmail.com>
2019-09-21 11:56:22 -0500

    detect wireless interface automatically

--- polybar/polybar/launch.sh
+++ polybar/polybar/launch.sh
@@ -6,10 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+wifi=$(echo /sys/class/net/*/wireless | awk -F'/' '{ print $5 }')
+
 # Launch polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload top &
+    WIRELESS_INTERFACE=$wifi MONITOR=$m polybar --reload top &
   done
 else
   polybar --reload top &
