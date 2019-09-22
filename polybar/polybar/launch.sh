7ca94dd8c7b9df6d33576b25e02f1b3c559cb434
Sean Lang <slang800@gmail.com>
2019-09-21 20:23:28 -0500

    autodetect ethernet interface for polybar

--- polybar/polybar/launch.sh
+++ polybar/polybar/launch.sh
@@ -7,11 +7,12 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 wifi=$(echo /sys/class/net/*/wireless | awk -F'/' '{ print $5 }')
+eth=$(ls /sys/class/net | grep ^e)
 
 # Launch polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    WIRELESS_INTERFACE=$wifi MONITOR=$m polybar --reload top &
+    WIRELESS_INTERFACE=$wifi ETHERNET_INTERFACE=$eth MONITOR=$m polybar --reload top &
   done
 else
   polybar --reload top &
